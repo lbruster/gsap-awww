dc4bdf55da047a36a12b9c2a362012cd22e1efea
Leroy B <shoked@gmail.com>
2025-10-05 23:40:17 -0500

    Initial second message2

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const FlavorTitle = () => {
   return (
     <div className="general-title col-senter h-full 2xl:gap-32 xl:gap-2 gap-16">
-      Flavor title
+      <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+        <h1>We have 6</h1>
+      </div>
+      <div style={{}} className=""></div>
     </div>
   );
 };
