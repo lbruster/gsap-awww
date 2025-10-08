86cd369760f6adc01471453d876c15cd3c2aa1f7
Leroy B <shoked@gmail.com>
2025-10-08 00:25:16 -0500

    Initial flavor slider error

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -6,6 +6,26 @@ import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const FlavorSlider = () => {
+  const sliderRef = useRef();
+
+  useGSAP(() => {
+    const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "2% top",
+        end: `+=${scrollAmount + 1500}px`,
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+    tl.to(".flavor-section", {
+      x: `-${scrollAmount + 1500}px`,
+      ease: "power1.inOut",
+    });
+  });
   return (
     <div className="slider-wrapper">
       <div className="flavors">
